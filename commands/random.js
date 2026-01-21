ae9fe26c63944263d9fd20b9b417af358ed6832f
Copvampire | Ed <copvampire@users.noreply.github.com>
2026-01-21 15:44:35 +0000

    Delete commands directory

--- commands/random.js
+++
@@ -1,8 +0,0 @@
-const Library = require('../Lib.js');
-module.exports = {
-    name: 'random',
-    description: 'Random Number!',
-    execute(client, config, dataFile, message, args) {
-        message.channel.send(Library.getRandomNumber(100));
-    },
-};
--- commands/server.js
+++
@@ -1,9 +0,0 @@
-const Library = require('../Lib.js');
-module.exports = {
-    name: 'server',
-    description: 'server info',
-    execute(client, config, dataFile, message, args) {
-        var guild = client.guilds.get(config.serverID);
-        message.channel.sendEmbed(Library.getServerMessage(guild));
-    },
-};
